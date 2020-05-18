44aad936c4a5237328fad094996f6d24a8e333b4
ppoote <ppoote@gmail.com>
2020-05-18 14:51:34 +0900

    added main

---
+++ src/components/Thirddata.js
@@ -0,0 +1,67 @@
+// import React from "react";
+// import styled from "styled-components";
+
+const Thirddata = [
+  {
+    title: "Sleep",
+    description: "Improve your",
+    imageURL:
+      "https://libraryitems.insighttimer.com/k5u7a8q4e7f5z8q9e4d1t1c3k1y1e9j7s3q1t1y7%2Fpictures%2Fsquare_medium.jpeg?alt=media",
+  },
+  {
+    title: "Anxiety",
+    description: "Coping with",
+    imageURL:
+      "https://libraryitems.insighttimer.com/g6y9w5z3q7a5s1k5t6j2s7h2b5x0p8u2c8h8s9n4%2Fpictures%2Fsquare_medium.jpeg?alt=media",
+  },
+  {
+    title: "Stress",
+    description: "Managing",
+    imageURL:
+      "https://libraryitems.insighttimer.com/e6a7u0k8g1d4q7t5q2x5v1s1n4n8p6z4e6x7y5n4%2Fpictures%2Fsquare_medium.jpeg?alt=media",
+  },
+  {
+    title: "Music",
+    description: "Meditation",
+    imageURL:
+      "https://libraryitems.insighttimer.com/k2g2s2f2n8c4n7k0f6p5d9p4h3p6h3n8x4f3l9t4%2Fpictures%2Fsquare_medium.jpeg?alt=media",
+  },
+  {
+    title: "Kids",
+    description: "Meditation for",
+    imageURL:
+      "https://libraryitems.insighttimer.com/g6t6e5a6y8g6c4w4a0q2x9g3w4e7w6d1g2v4k6t4%2Fpictures%2Fsquare_medium.jpeg?alt=media",
+  },
+  {
+    title: "Kids",
+    description: "Meditation for",
+    imageURL:
+      "https://libraryitems.insighttimer.com/w8b3s6j3y4z8e9e3h6q6r7s7x1a4y3m9q5e6q0y9%2Fpictures%2Fsquare_medium.jpeg?alt=media",
+  },
+  {
+    title: "Kids",
+    description: "Meditation for",
+    imageURL:
+      "https://libraryitems.insighttimer.com/u5h5r4g8l9f0l0h8g2z6f3x8w8k6z6b1e9r2c2r7%2Fpictures%2Fsquare_medium.jpeg?alt=media",
+  },
+  {
+    title: "Kids",
+    description: "Meditation for",
+    imageURL:
+      "https://libraryitems.insighttimer.com/g6t6e5a6y8g6c4w4a0q2x9g3w4e7w6d1g2v4k6t4%2Fpictures%2Fsquare_medium.jpeg?alt=media",
+  },
+  {
+    title: "Kids",
+    description: "Meditation for",
+    imageURL:
+      "https://libraryitems.insighttimer.com/z6s0j2b4t6t6c0v5r8c8h8n7g2q6s2h8t8z6p2d3%2Fpictures%2Fsquare_medium.jpeg?alt=media",
+  },
+  {
+    title: "Kids",
+    description: "Meditation for",
+    imageURL:
+      "https://libraryitems.insighttimer.com/f7g9v7p7k6l9t7r2a8m2c6z0w8s1v6b4v3v8p6y2%2Fpictures%2Fsquare_medium.jpeg?alt=media",
+  },
+];
+
+export default Thirddata;
