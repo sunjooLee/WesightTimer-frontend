30e508c07f26d87b0c39d392b31f30b15ef9910e
Sun ju Lee <yohekisa@gmail.com>
2020-05-22 15:09:13 +0900

    added final stress

--- src/pages/StressDetail.js
+++ src/pages/StressDetail.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import styled from 'styled-components';
 import Nav from '../components/Nav';
 import AppImg from "../images/course_img.png"
+import ReviewCard from './Review';
 
 const StressDetail = () => {
 
@@ -122,6 +123,7 @@ const StressDetail = () => {
         return () => clearInterval(timer);
     }, [isPlaying, currentTime]);
 
+    console.log(reviews);
 
     return (
         <div>
@@ -242,29 +244,7 @@ const StressDetail = () => {
                 <ReviewWrapper>
                 <ReviewSlider moveSlider={translateVal}>
                 {reviews && reviews.map(item => (
-                    <Review>
-                    <HeaderDiv>
-                    <ImgCont/>
-                    <DivCont>
-                    <DivUserName>
-                        {item.username}
-                    </DivUserName>
-                    <DivDate>
-                        {item.write_date.substring(0,10)}
-                    </DivDate>
-                    </DivCont>
-                    <StarsDiv>
-                        <Stars><svg class="MuiSvgIcon-root w-3 h-3 text-black75" focusable="false" viewBox="0 0 24 24" aria-hidden="true" role="presentation"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path><path fill="none" d="M0 0h24v24H0z"></path></svg></Stars>
-                        <Stars><svg class="MuiSvgIcon-root w-3 h-3 text-black75" focusable="false" viewBox="0 0 24 24" aria-hidden="true" role="presentation"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path><path fill="none" d="M0 0h24v24H0z"></path></svg></Stars>
-                        <Stars><svg class="MuiSvgIcon-root w-3 h-3 text-black75" focusable="false" viewBox="0 0 24 24" aria-hidden="true" role="presentation"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path><path fill="none" d="M0 0h24v24H0z"></path></svg></Stars>
-                        <Stars><svg class="MuiSvgIcon-root w-3 h-3 text-black75" focusable="false" viewBox="0 0 24 24" aria-hidden="true" role="presentation"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path><path fill="none" d="M0 0h24v24H0z"></path></svg></Stars>
-                        <Stars><svg class="MuiSvgIcon-root w-3 h-3 text-black75" focusable="false" viewBox="0 0 24 24" aria-hidden="true" role="presentation"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path><path fill="none" d="M0 0h24v24H0z"></path></svg></Stars>
-                    </StarsDiv>
-                    </HeaderDiv>
-                    <ReviewContent>
-                    {item.reviewContent}
-                    </ReviewContent>
-                </Review>
+                    <ReviewCard userName={item.username} writeDate={item.write_date.substring(0,10)} reviewContent={item.reviewContent}/>
                 ))}
                 </ReviewSlider>
                 </ReviewWrapper>
@@ -694,15 +674,6 @@ const ReviewSlider = styled.div`
     transition: transform 0.4s ease-out;
 `;
 
-const Review = styled.div`
-    width: 50%;
-    margin-right: 20px;
-    background-color: #f5f5f5;
-    height: max-content;
-    margin-bottom: 6rem;
-    display: flex;
-    flex-direction: column;
-`;
 
 const ReviewWrapper = styled.div`
     margin: 2rem 0;
@@ -710,61 +681,6 @@ const ReviewWrapper = styled.div`
     overflow: hidden;
     width: 100%;
     height: inherit;
-`;
-
-const ReviewContent = styled.p`
-    padding: 20px;
-    letter-spacing: .025em!important;
-    color: #1a1a1a;
-    line-height: 1.625!important;
-    font-family: ProximaNovaRegular;
-
-`;
-
-const HeaderDiv = styled.div`
-    display: flex;
-    padding: 20px;
-`;
-
-const ImgCont = styled.div`
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    padding: 10px;
-    background-image: url("https://users.insighttimer.com/D3V6k3e9t4J6L0d8R0n3B6c0D1W7Y9e6A7r0z1z0W9N5q2P2q9q1r5a3V1R8D4Y6q4s5P4U6V4w2t6V6G2Q6y2x1d5U2n9P7C6M9%2Fpictures%2Fsquare_medium.jpeg?alt=media");
-    background-size: cover;
-    background-position: center;
-`;
-
-const DivCont = styled.div`
-    display:flex;
-    height: 40px;
-    width: 324.5px;
-    flex-direction: column;
-    justify-content: space-around;
-    padding: 0px 10px;
-`;
-
-const DivUserName = styled.div`
-    line-height: 1;
-    font-family: ProximaNovaSemibold;
-`;
-
-const DivDate = styled.div`
-    color: #7b7b7b;
-    font-family: ProximaNovaRegular;
-    font-size: 12px;
-`;
-
-const StarsDiv = styled.div`
-    width: 100px;
-    height: 40px;
-    padding: 10px;
-`;
-
-const Stars = styled.svg`
-    width: 12px;
-    height: 12px;
 `;
 
 const LastDiv = styled.div`
